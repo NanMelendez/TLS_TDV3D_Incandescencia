f40592a57967511895f4a00d8bbd907e2c746d1a
Nander Melendez <64971585+NanMelendez@users.noreply.github.com>
2026-09-20 14:53:33 -0500

    Superficie secreta

---
+++ Assets/Scripts/Environment/SurfaceReveal.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SurfaceReveal : MonoBehaviour
+{
+    [SerializeField] private Material mat;
+    [SerializeField] private Light spotLight;
+
+    private void Update()
+    {
+        mat.SetVector("_SpotPos", spotLight.transform.position);
+        mat.SetVector("_SpotDir", -spotLight.transform.forward);
+        mat.SetFloat("_SpotAngle", spotLight.spotAngle);
+    }
+}
